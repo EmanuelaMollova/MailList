eeb7b8f6f9d5fb2ed1ba5768d69b8d1bbb4c5a79
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-18 20:29:36 +0200

    Add create function and tests

--- mail.py
+++ mail.py
@@ -1,10 +1,16 @@
 class Mail():
-    def __init(mail_list):
-        mail_list = {}
+    def __init__(self):
+        self.mail_list = {}
+        self.register = {}
 
     def choose_action(self, command):
         if command == 'help':
             self.help()
+        if commnad == 'create':
+            self.create()
+
+    def say_hello(self):
+        print("Hello Stranger! This is a cutting-edge, console-based mail-list!\nType help, to see a list of commands.")
 
     def help(self):
         messages = [
@@ -13,7 +19,7 @@ class Mail():
             "* show_list <unique_list_identifier> - Prints all people, one person at a line, that are subscribed for the list. The format is: <Name> - <Email>",
             "* add <unique_list_identifier> - Starts the procedure for adding a person to a mail list. The program prompts for name and email.",
             "* create <list_name> - Creates a new empty list, with the given name.",
-            "* search_email <email> - Performs a search into all lists to see if the given email is present. Shows all lists, where the email was found.",
+            "* search_email <email> - Performs a search into all lists to see if theunique_list_identifier given email is present. Shows all lists, where the email was found.",
             "* merge_lists <list_identifier_1> <list_identifier_2> <new_list_name> - merges list1 and list2 into a new list, with the given name.",
             "* export <unique_list_identifier> - Exports the given list into JSON file, named just like the list. All white spaces are replaced by underscores.",
             "* exit - this will quit the program"
@@ -21,8 +27,19 @@ class Mail():
 
         print("\n".join(messages))
 
-    def say_hello(self):
-        print("Hello Stranger! This is a cutting-edge, console-based mail-list!\n Type help, to see a list of commands.")
+    def create(self, list_name):
+        if list_name in self.mail_list:
+            return "A mail list with this name already exists!"
+        else:
+            if len(self.register.keys()) == 0:
+                key = 1
+            else:
+                key = max(self.register.keys()) + 1
+
+            self.register[key] = list_name
+
+        self.mail_list[list_name] = []
+        return "New list <{}> was created".format(list_name)
 
 def main():
     mail = Mail()
--- mail_tests.py
+++ mail_tests.py
@@ -5,7 +5,16 @@ import unittest
 class MailTest(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.m = mail.Mail()
+
+    def test_create(self):
+        self.assertEqual("New list <HackBG> was created", self.m.create("HackBG"))
+        self.assertEqual("HackBG", self.m.register[1])
+        self.assertEqual([], self.m.mail_list["HackBG"])
+        self.assertEqual("A mail list with this name already exists!", self.m.create("HackBG"))
+        print(self.m.register)
+        print(self.m.mail_list)
+
 
 if __name__ == '__main__':
     unittest.main()
